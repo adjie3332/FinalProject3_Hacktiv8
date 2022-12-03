03672af7d328431b560c0928bc7596842dab1c2c
adjie3332 <adjie3332@gmail.com>
2022-12-03 20:41:33 +0700

    FinalProject3

---
+++ script.js
@@ -0,0 +1,9 @@
+// Text Typed
+var options = {
+    strings: ['coding with us,', 'design with us,', 'to be creative with us.'],
+    typeSpeed: 70,
+    loop: true,
+  };
+  
+  var typed = new Typed('.typed', options);
+  
